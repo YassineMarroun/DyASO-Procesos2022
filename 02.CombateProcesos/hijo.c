2f4fd4623ae28f6411b95bccbd01bd366c981535
YassineMarroun <el.yasin@hotmail.com>
2021-12-09 11:08:22 +0100

    Commit

--- 02.CombateProcesos/hijo.c
+++ 02.CombateProcesos/hijo.c
@@ -1,13 +1,15 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
-#include <string.h>
-#include <unistd.h>
 #include <sys/msg.h>
-#include <sys/wait.h>
+
+
 
 int main(int argc, char *argv[]) {
     printf("Ejecutándo hijo: ", getpid());
